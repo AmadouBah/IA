de9a01a269324d8e7cae9ed7f897a7a1bbf5f9ad
Bah Amadou <mrbah75@gmail.com>
2018-10-21 20:42:34 +0200

    modif problem

--- search/Problem.java
+++ search/Problem.java
@@ -1,5 +1,6 @@
 package search;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -19,19 +20,6 @@ public class Problem<State extends Searchable<State,Action>,Action> {
 	public boolean goal_test(State s){
 		return finalState.equals(s);
 	}
-
-	/**
-	 * returns all possible actions from the current state
-	 * @return a list of all possible actions from the current state
-	 */
-	public List<Action> getActions();
-	
-	/**
-	 * the method return the state resulting from taking action <code>a</code> in the current state
-	 * @param a is the action taken
-	 * @return the state resulting from taking action a in the current state
-	 */
-	public State execute(Action a);
 	
 	public Problem(State start, State stop){
 		initialState = start;
@@ -43,53 +31,120 @@ public class Problem<State extends Searchable<State,Action>,Action> {
 	}
 	
 	public int bfs(){
-		
-			List<State> visited = new ArrayList<>();
-			List<State> file  =  new ArrayList<>();
-			State currentState = this.initialState;
+		State current = this.initialState;
+		List<State> dejavu = new ArrayList<State>();
+		List<State> avisiter = new ArrayList<State>();
+		List<Action> actions = new ArrayList<>();
+		avisiter.add(current);
+		int cpt=0;
+		System.out.println(current);
+		System.out.println("debut de bfs!");
+
+		while(!goal_test(current)) {
 			
-			int cpt=0;
-			
-			visite.add(initialState);
-
-			while(currentState!=finalState)
-				
-				List<Action> actions = initialState.getActions();
-
-				for(Action a : actions){
-					
-					State s = execute(a);
-					if(!visited.contains(s)){
-						file.add(s);
-					}
+			actions = current.getActions();
+			State fils;
+			for(Action a: actions) {
+				fils = current.execute(a);
+				if(!dejavu.contains(fils)&& !avisiter.contains(fils)){
+					avisiter.add(fils);
 				}
-				
-				while(!file.isEmpty()){
-					currentState = file.get(0);
-					actions = currentState.getActions();
-
-				}
-
-					
-					
-
-				}
-
-
 			}
+			dejavu.add(current);
+			avisiter.remove(current);
+			current = avisiter.get(0);
+			cpt++;
+		}
+		System.out.println(current);
 
-                return 0;
-    }
+		System.out.println("nb noeuds visites:"+cpt );
+		return cpt;
+	}
 	
 	
 	public int dfs(){
-		// to be completed
-        return 0;
+		State current = this.initialState;
+		List<State> dejavu = new ArrayList<State>();  //liste des etats deja visités
+		Stack<State> avisiter = new Stack<State>();   //liste des etats a visiter
+		List<Action>actions  = new ArrayList<>();
+		avisiter.push(current);
+		
+		int cpt=0;
+		System.out.println(current);
+
+		System.out.println("debut de dfs !");
+		while(!goal_test(current)) {
+			
+			actions = current.getActions();
+			State fils;
+			
+			for(Action a: actions) {
+				fils = current.execute(a);
+				if(!dejavu.contains(fils)){
+					avisiter.push(fils);
+				}
+			}
+			
+			dejavu.add(current);
+			avisiter.remove(current);
+			current = avisiter.peek();
+			cpt++;
+		}
+		System.out.println(current);
+
+		System.out.println("nb noeuds visites : "+cpt);
+
+		return cpt;
     }
 	
+	public int getIndiceMin(List<Integer>list) {
+		int min =10000000;
+		int indiceMin = 0;
+		
+		for(Integer val: list) {
+			if(val<min) {
+				min=val;
+				indiceMin = list.indexOf(val);
+			}
+		}
+		return  indiceMin;
+	}
+	
+	
 	public int aStar(Function<State,Double> g, Function<State,Double> h ){
-		// to be completed
-        return 0;
+		
+		State current = initialState;
+		List<State> avisiter = new ArrayList<>();
+		List<State> dejavu = new ArrayList<>();
+		List<Integer>couts = new ArrayList<>();
+		List<Action> actions = new ArrayList<>();
+		int cpt=0;
+		int f;
+		int indicemin;
+		while(!goal_test(current)) {
+			
+			actions = current.getActions();
+			for(Action a : actions) {
+				State fils = current.execute(a);
+				
+				if(!dejavu.contains(fils)) {
+					fils.setValueG(fils.getValueG()+fils.getPredecessor().getValueG());
+					f=fils.getHeuristic()+fils.getValueG();
+					avisiter.add(fils);
+					couts.add(f);
+				}
+			}
+			
+			indicemin=this.getIndiceMin(couts);
+			dejavu.add(current);
+			avisiter.remove(current);
+			current = avisiter.get(indicemin);
+			
+			cpt++;
+		}
+		
+		
+		return cpt;
     }
 
 }
